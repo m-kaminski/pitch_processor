62cc6bf1d05d615b90645970b1191192462133bf
Matt (Maciej) Kaminski <maciej.a.kaminski@gmail.com>
2023-03-07 15:33:08 -0600

    Cleanup formatting on main

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,30 @@
 #include "pitch_decoder.h"
 #include "event_accumulator.h"
 
-namespace pitchstream {
+namespace pitchstream
+{
     /***
      * Process data in PITCH format from standard input,
      * print summary to standard output
-    */
-    void process_input() {
+     */
+    void process_input()
+    {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(NULL);
         event_accumulator a;
         std::string s;
-        while (std::getline(std::cin, s)) {
+        while (std::getline(std::cin, s))
+        {
             // for each event
             a.process_message(std::move(
                 pitch_decoder::decode(s.begin(), s.end())));
         }
-        format_summary(std::cout , a.generate_summary_n(10));
+        format_summary(std::cout, a.generate_summary_n(10));
     }
 }
 
 int main(int argc, char **argv)
 {
-    std::ios_base::sync_with_stdio (false);
-    std::cin.tie(NULL);
+
     pitchstream::process_input();
 }
